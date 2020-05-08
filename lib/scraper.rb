32fb4e8141106b4307721b0556cf359b3a6fc5a9
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-08 14:32:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+#html = open("https://flatironschool.com/")
 
-Nokogiri::HTML(html)
-doc = Nokogiri::HTML(html)
+#Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open(https://flatironschool.com/))
