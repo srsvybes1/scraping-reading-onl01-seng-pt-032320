a54a9e9943abee2b7dc95a855e7e165679a75c26
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-08 14:45:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,3 +6,5 @@ require 'open-uri'
 #Nokogiri::HTML(html)
 doc = Nokogiri::HTML(open(https://flatironschool.com/))
 doc.css(".headline-26OIBN").text
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
