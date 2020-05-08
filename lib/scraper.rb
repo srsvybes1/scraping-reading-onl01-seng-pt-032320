1a67ce29eb2e8e7f538df8d53e6e6342792b950e
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-08 14:47:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+#html = open("https://flatironschool.com/")
+
+#Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open(https://flatironschool.com/))
+doc.css(".headline-26OIBN").text
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
